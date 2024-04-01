798347ab28da95e11d6b7630a19814043a50cbd2
Javier Montes <javier.montes.carrera@gmail.com>
2024-04-01 09:49:34 +0000

    First commit

---
+++ app/api/chat/route.ts
@@ -0,0 +1,28 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+const openai = new OpenAI({
+  baseURL: "http://127.0.0.1:5000/v1",
+});
+
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: [
+      {
+        role: "system",
+        content:
+          `You are a professional comedian who has been hired to write a series of jokes. Just answer with the joke you were asked to return`,
+      },
+      ...messages,
+    ],
+  });
+
+  const stream = OpenAIStream(response);
+  return new StreamingTextResponse(stream);
+}
---
+++ app/page.tsx
@@ -0,0 +1,153 @@
+"use client";
+
+import { useState } from "react";
+import { useChat } from "ai/react";
+
+export default function Chat() {
+  const { messages, append, isLoading } = useChat();
+  const genres = [
+    { emoji: "👨🏻‍💻", value: "Work" },
+    { emoji: "👩‍🦰🧑‍🦰", value: "People" },
+    { emoji: "🐶😺", value: "Animals" },
+    { emoji: "🌮", value: "food" },
+    { emoji: "📺", value: "television" },
+  ];
+  const tones = [
+    { emoji: "💡", value: "witty" },
+    { emoji: "😜", value: "sarcastic" },
+    { emoji: "🤓", value: "silly" },
+    { emoji: "😎", value: "dark" },
+    { emoji: "🤪", value: "goofy" },
+  ];
+  const kinds = [
+    { emoji: "🔠", value: "pun" },
+    { emoji: "🚪", value: "knock-knock" },
+    { emoji: "📋", value: "story" },
+  ];
+
+  const [state, setState] = useState({
+    genre: "",
+    tone: "",
+    kind: "",
+    temperature: "",
+  }); 
+
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  return (
+    <main className="mx-auto w-full p-24 flex flex-col">
+      <div className="p4 m-4">
+        <div className="flex flex-col items-center justify-center space-y-8 text-white">
+          <div className="space-y-2">
+            <h2 className="text-3xl font-bold">Jokes App</h2>
+            <p className="text-zinc-500 dark:text-zinc-400">
+              Customize the jokes by selecting the genre, tone and kind.
+            </p>
+          </div>
+
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Genre</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {genres.map(({ value, emoji }) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    value={value}
+                    name="genre"
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${emoji} ${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Tones</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {tones.map(({ value, emoji }) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    name="tone"
+                    value={value}
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${emoji} ${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Kinds</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {kinds.map(({ value, emoji }) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    name="kind"
+                    value={value}
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${emoji} ${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
+            disabled={isLoading || (!state.genre || !state.tone || !state.kind)}
+            onClick={() =>
+              append({
+                role: "user",
+                content: `Generate a ${state.genre} joke, the kind of joke you will generate is: ${state.kind}. The tone used must be: ${state.tone}`,
+              })
+            }
+          >
+            Generate Joke
+          </button>
+
+          <div
+            hidden={
+              messages.length === 0 ||
+              messages[messages.length - 1]?.content.startsWith("Generate")
+            }
+            className="bg-opacity-25 bg-gray-700 rounded-lg p-4"
+          >
+            {messages[messages.length - 1]?.content}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
