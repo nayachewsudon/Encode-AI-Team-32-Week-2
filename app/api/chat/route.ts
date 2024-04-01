d7aa16054af0ef3b25845216c6c7a2ac847c5501
Kanaya Ozora <163905596+nayachewsudon@users.noreply.github.com>
2024-04-01 14:36:44 +0200

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,6 +2,7 @@ import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 
 const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
   baseURL: "http://127.0.0.1:5000/v1",
 });
 
@@ -19,9 +20,16 @@ export async function POST(req: Request) {
         content:
           `You are a professional comedian who has been hired to write a series of jokes. Just answer with the joke you were asked to return`,
       },
-      ...messages,
+      ...messages.additionalMessages,
+        {
+          role: "system",
+          content:
+            `Based on the generated joke, evaluate and classify if the joke 1) is funny or not, 2) appropriate or not, 3) offensive or not, 4) a dad joke or not `,
+        },
+        ...messages,
     ],
   });
+
 
   const stream = OpenAIStream(response);
   return new StreamingTextResponse(stream);
